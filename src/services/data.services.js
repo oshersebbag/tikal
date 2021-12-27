1e236085197ea0b7e248058308c90bb0f184bf42
osher <osher.sabag@gmail.com>
2021-12-27 17:29:02 +0200

    final

--- src/services/data.services.js
+++ src/services/data.services.js
@@ -1,8 +1,5 @@
 class DataService {
 
-    ///////////////////// services for the Top-Vehicle task (task No. 1) ///////////////////
-
-    // getting the combined data for all vehicles, sorting them by their sum population - and sending all the vehicles that has the highest sum of population
     getTopVehicle = async () => {
         const combinedData = await this.getCombinedData();
         combinedData.sort((a, b) => {
@@ -16,7 +13,6 @@ class DataService {
         return combinedData.filter((item) => item.sumPopulation >= combinedData[0].sumPopulation);
     }
 
-    // fetching all the data from each category and creating the combined data
     getCombinedData = async () => {
         const [vehicles, pilots, planets] = await Promise.all([
             this.fetchByCategory("vehicles"),
@@ -27,7 +23,6 @@ class DataService {
         return combinedData;
     }
 
-    // getting all the data of a category - vehicles/people/planets
     fetchByCategory = async (category) => {
         let URL = "https://swapi.dev/api/" + category;
         const categoryData = [];
@@ -45,9 +40,6 @@ class DataService {
         return categoryData;
     }
 
-
-    // recieving the data from all 3 categories, getting a map obj with the URL key and the rspected info needed
-    // and creating the combined data with the info of the vehicle, pilots, planets and the sum of population
     createCombinedData(vehicles, pilots, planets) {
 
         const pilotsMap = this.mapDataByURL(pilots, ["name", "homeworld"]);
@@ -65,8 +57,6 @@ class DataService {
         return finalData;
     }
 
-
-    // creates a map obj (used for planets and pilots - each needs different info so I used a "keys" array)
     mapDataByURL(items, keys) {
         const mappedData = {};
         if (items.length === 0) { return mappedData; }
@@ -81,7 +71,6 @@ class DataService {
         }
     }
 
-    // returning an array with the info we need on the pilots for the combined data 
     getPilots(pilots, map) {
         const res = [];
         pilots.forEach((pilot) => {
@@ -90,23 +79,17 @@ class DataService {
         return res;
     }
 
-    // returning an array with the info we need on the planets for the combined data 
     getHomeworlds(pilots, map) {
         const res = [];
         pilots.forEach((pilot) => res.push(map[pilot.homeworld]));
         return res;
     }
 
-    // returning the sum of population for the planets 
     getSum(planets) {
         return planets.reduce((total, planet) => total + parseInt(planet.population), 0);
 
     }
 
-    /////////////////// services for the Population-Chart task (task No. 2) /////////////////// 
-
-    // getting a list of planets and using the api search to recieve only these planets, sorting them 
-    // and sending them with their heights in log scale compared to the highest population amongst them
     getChartData = async (list) => {
         const planets = [];
         for (const planet of list) {
@@ -128,7 +111,6 @@ class DataService {
         return this.getHeight(planets);;
     }
 
-    //getting a sorted array of planets and adding a log-scaled height property(for the population chart)   
     getHeight = (planets) => {
         const myFixedheight = 250;
         const logMaxPopulation = Math.log10(parseInt(planets[0].population));
